c097a5532cac8a55ac5c883cf9eb2afec09fc56b
Wojciech Bartnik <yisonPylkita@gmail.com>
2021-07-05 22:19:53 +0200

    LDA implemented

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use anyhow::{anyhow, Result};
+
 pub struct Cpu {
     pub reg_a: u8,
     pub status: u8,
@@ -12,11 +14,45 @@ impl Cpu {
             pc: 0,
         }
     }
+
+    pub fn execute(&mut self, code: Vec<u8>) -> Result<()> {
+        if code.len() > u16::MAX as usize {
+            return Err(anyhow!("Code to execute is too big"));
+        }
+        self.pc = 0;
+        loop {
+            if self.pc >= code.len() as u16 {
+                break;
+            }
+            let opcode = code[self.pc as usize];
+            self.pc += 1;
+            match opcode {
+                0xa9 => {
+                    self.reg_a = code[self.pc as usize];
+                    self.pc += 1;
+                    if self.reg_a == 0x00 {
+                        self.status |= 0b0000_0010;
+                    } else {
+                        self.status &= 0b1111_1101;
+                    }
+                    if self.reg_a & 0b1000_0000 != 0 {
+                        self.status |= 0b1000_0000;
+                    } else {
+                        self.status &= 0b0111_1111;
+                    }
+                }
+                _ => todo!(),
+            }
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use assert::assert_ok;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn cpu_after_init_should_have_all_registers_clear() {
@@ -24,5 +60,27 @@ mod test {
         assert_eq!(cpu.reg_a, 0);
         assert_eq!(cpu.status, 0);
         assert_eq!(cpu.pc, 0);
+    }
+
+    #[test]
+    fn execute_lda_with_zero() {
+        let mut cpu = Cpu::new();
+        assert_ok!(cpu.execute(vec![0xa9, 0x00]));
+
+        // TODO: name status bits properly
+        assert_eq!(cpu.reg_a, 0x00);
+        assert_eq!(cpu.status, 0x02);
+        assert_eq!(cpu.pc, 2);
+    }
+
+    #[test]
+    fn execute_lda_with_value_with_bit_seven_seyt() {
+        let mut cpu = Cpu::new();
+        assert_ok!(cpu.execute(vec![0xa9, 0x80]));
+
+        // TODO: name status bits properly
+        assert_eq!(cpu.reg_a, 0x80);
+        assert_eq!(cpu.status, 0x80);
+        assert_eq!(cpu.pc, 2);
     }
 }
